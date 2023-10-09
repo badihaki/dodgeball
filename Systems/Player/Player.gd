613594d9f739adbdd10b4ec5e986871ab5e83c73
badihaki <jojackblack@gmail.com>
2023-10-09 02:07:44 -0400

    The team is spawning onto the field

--- Systems/Player/Player.gd
+++ Systems/Player/Player.gd
@@ -3,9 +3,39 @@ class_name Player
 
 var input_actions:PlayerInputs
 
+@export_category("Team Settings")
+@export var is_left_field_team:bool
+@export var team_members:Array[PackedScene]
+@export var field_characters:Array[Character]
+@export var field_char_index:int = 0
+
 func _ready() -> void:
 	input_actions = $Inputs
-	input_actions.pass_action.connect(OnPass)
+	BuildTeamGameplay()
 
-func OnPass()->void:
-	print("passing at"+Time.get_date_string_from_system())
+func BuildTeamGameplay()->void:
+	if team_members.size() < 3:
+		printerr("Need more members for team")
+		return
+	elif team_members.size() > 3:
+		printerr("Too many team members, slicing array elements 0-3")
+		var new_team_members = team_members.slice(0,3)
+		team_members = new_team_members
+		_PutCharactersOnField()
+	elif team_members.size() == 3:
+		_PutCharactersOnField()
+
+func _PutCharactersOnField()->void:
+	var spawn_points:Array[Node] = get_node("../Spawn").get_children()
+	while field_characters.size() != team_members.size():
+		var char_build = team_members[field_char_index].instantiate()
+		char_build.controlling_player = self
+		add_child(char_build)
+		char_build.position = spawn_points[field_char_index].position
+		field_characters.append(char_build)
+		field_char_index += 1
+	field_char_index = 0
+	GiveControlToCharacterIndex()
+
+func GiveControlToCharacterIndex()->void:
+	field_characters[field_char_index].is_controlled_by_player = true
